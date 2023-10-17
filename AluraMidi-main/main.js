913f00c1cbca40f878abe168cb936356133a3bf6
amanda2512 <126569036+amanda2512@users.noreply.github.com>
2023-10-17 07:51:52 -0300

    Update main.js

--- AluraMidi-main/main.js
+++ AluraMidi-main/main.js
@@ -1,3 +1,44 @@
-function tocaSomPom(){
-document.querySelector('#som_tecla_pom').play();
+function TocaSomPom(){
+document.querySelector('.tecla_pom').onclik= tocaSomPom;
 }
+document.querySelector('#som_tecla_pom').play();
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_clap').play();
+}
+document.querySelector('.tecla_clap').onclik= tocaSomClap;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_tim').play();
+}
+document.querySelector('.tecla_tim').onclik= tocaSomTim;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_puff').play();
+}
+document.querySelector('.tecla_puff').onclik= tocaSomPuff;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_splash').play();
+}
+document.querySelector('.tecla_splash').onclik= tocaSomSplash;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_toim').play();
+}
+document.querySelector('.tecla_toim').onclik= tocaSomToim;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_pshi').play();
+}
+document.querySelector('.tecla_pshi').onclik= tocaSomPshi;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_tic').play();
+}
+document.querySelector('.tecla_tic').onclik= tocaSomTic;
+
+function TocaSomPom(){
+document.querySelector('#Som_tecla_tom').play();
+}
+document.querySelector('.tecla_tom').onclik= tocaSomTom;
